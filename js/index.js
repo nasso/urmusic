95d117730f6047f35e7d20c6a8ec780ad3646a26
Nasso <nassomails@gmail.com>
2016-09-17 13:24:46 +0200

    New compression

--- js/index.js
+++ js/index.js
@@ -1,6 +1,51 @@
 // TAB_SIZE = 4
 
 // Utils
+// object.watch
+if (!Object.prototype.watch) {
+	Object.defineProperty(Object.prototype, "watch", {
+		  enumerable: false
+		, configurable: true
+		, writable: false
+		, value: function (prop, handler) {
+			var
+			  oldval = this[prop]
+			, newval = oldval
+			, getter = function () {
+				return newval;
+			}
+			, setter = function (val) {
+				oldval = newval;
+				return newval = handler.call(this, prop, oldval, val);
+			}
+			;
+			
+			if (delete this[prop]) { // can't watch constants
+				Object.defineProperty(this, prop, {
+					  get: getter
+					, set: setter
+					, enumerable: true
+					, configurable: true
+				});
+			}
+		}
+	});
+}
+
+// object.unwatch
+if (!Object.prototype.unwatch) {
+	Object.defineProperty(Object.prototype, "unwatch", {
+		  enumerable: false
+		, configurable: true
+		, writable: false
+		, value: function (prop) {
+			var val = this[prop];
+			delete this[prop]; // remove accessors
+			this[prop] = val;
+		}
+	});
+}
+
 function isNullOrUndef(v) { return (v === null || v === undefined); }
 
 function EnumerationValue(ownerEnum, name) {
@@ -212,7 +257,7 @@ var frameProps = {
 	minlowval: Number.MIN_SAFE_INTEGER,
 	
 	maxhighval: 0,
-	minhighval: Number.MIN_SAFE_INTEGER,
+	minhighval: Number.MIN_SAFE_INTEGER
 };
 
 function loadFilePreset(f, setIt) {
