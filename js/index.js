5c82f6028d19719c0eb00cf89eed5c567c6e6f8c
Nasso <nassomails@gmail.com>
2016-09-25 12:55:47 +0200

    Updated version number

--- js/index.js
+++ js/index.js
@@ -1733,7 +1733,7 @@ window.addEventListener('load', function() {
 		+	"| |  | |  ____\\ \\   / / | |\t" +	"Hey you! This app is highly customizable through the JavaScript\n"
 		+	"| |__| | |__   \\ \\_/ /  | |\t" +	"console too! Have fun, and try not to broke everything :p!\n"
 		+	"|  __  |  __|   \\   /   | |\t" +	"\n"
-		+	"| |  | | |____   | |    |_|\t" +	"Urmusic V1.3.2\n"
+		+	"| |  | | |____   | |    |_|\t" +	"Urmusic V1.3.3\n"
 		+	"|_|  |_|______|  |_|    (_)\t" +	"By Nasso (https://nasso.github.io/)\n\n");
 		
 		loadPreset();
